d98c7df032e6085148c05f9ea46788f21af356c7
class1cal <nikaarudashviliyt2008@gmail.com>
2022-09-28 17:14:01 +0400

    second commit of group 102

---
+++ day2/lesson2.py
@@ -0,0 +1,23 @@
+# name = "nika"
+
+# print(len(name)) #len - Lenght 
+#           #4
+
+# my_sentance = """გამარჯობა, მე ვსწავლობ პროგრამირებას,
+# პროგრამირების ენას რომელსაც მე ვსწავლობ
+# ჰქვია პითონი."""
+
+# print(my_sentance)
+
+# knows_programming = True
+
+if 10>5: #True  -   10<5 = False
+      print("Hello")
+      # tab - ის საშუალებით მობახდინეთ ინდენტაცია. (indentation)
+
+if 15<2:
+      print("Nika")
+
+if 2==2: #equal
+      print("""lorem ipsum dolor
+      sit amet""")
